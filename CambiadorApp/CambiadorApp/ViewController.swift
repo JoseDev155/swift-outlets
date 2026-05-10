daf1659c7646ab8ceb8b372cd7a505751f65f749
Jose Developer <jeduramos7001@gmail.com>
2026-05-10 01:48:44 -0600

    Feat: Implement code in CambiadorApp

--- CambiadorApp/CambiadorApp/ViewController.swift
+++ CambiadorApp/CambiadorApp/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ColorViewController: UIViewController {
+
+    // MARK: - Outlets
+    // Conectamos la vista principal o un UIView cuadrado en el centro
+    @IBOutlet weak var cajaColorView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Podemos usar el outlet para configuración inicial también
+        cajaColorView.layer.cornerRadius = 10
     }
 
-
+    // MARK: - Actions
+    // Nota: Puedes conectar múltiples botones a una sola acción
+    @IBAction func cambioColorTapped(_ sender: UIButton) {
+        if sender.tag == 0 { // Botón Rojo
+            cajaColorView.backgroundColor = .red
+        } else if sender.tag == 1 { // Botón Azul
+            cajaColorView.backgroundColor = .blue
+        } else { // Botón Verde
+            cajaColorView.backgroundColor = .green
+        }
+    }
 }
-
