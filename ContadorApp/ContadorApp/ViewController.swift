c7b2cf35de6f76436d579656bfcf5a2172dbed08
Jose <jeduramos7001@gmail.com>
2026-05-10 01:58:01 -0700

    Add ContadorApp project base

---
+++ ContadorApp/ContadorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContadorApp
+//
+//  Created by Jose Ramos on 10/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
