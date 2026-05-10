637b6728946e435d90dc01ce23bbb46b18f22f32
Jose <jeduramos7001@gmail.com>
2026-05-10 02:45:23 -0700

    Add ValiadorApp project base

---
+++ ValidadorApp/ValidadorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ValidadorApp
+//
+//  Created by Jose Ramos on 10/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
